d813628eaf9bf7b7bfc7b46a34da992a7b82186e
brendan <brendan.whelan@sydney.edu.au>
2021-04-10 09:38:50 +1000

    Added in logging and some rudimentary error checking

--- modules/make_scan_params_file.py
+++ modules/make_scan_params_file.py
@@ -6,6 +6,28 @@
 #3) Run this file from the /basic_gp_stuff/ directory.
 
 import numpy as np
+import logging
+logging.basicConfig(format='[%(filename)s: line %(lineno)d] %(message)s', level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+
+
+def CheckScanParams(paramsfile):
+    """
+    Add in error handling here to check for correct dimensionality of various parameters
+
+    :param paramsfile:
+    :return:
+    """
+
+    # check for correction dimensionality of gp_precisionmatrix:
+    if not paramsfile['gp_precisionmat'].shape[1] == len(my_scan_params['dev_ids']):
+        logger.error('incorrect number of parameters detected in gp_precisionmat')
+
+    if not len(my_scan_params['start_point'])  == len(my_scan_params['dev_ids']):
+        logger.error('Number of device ids and starting parameters dont match')
+
+
 
 filename = 'TopasOptimiserParams.npy'
 
@@ -24,12 +46,14 @@ my_scan_params['gp_amp'] = 1.0
 my_scan_params['gp_noise'] = 0.01 
 
 #gp precision matrix, 2d-array of shape ndim x ndim where ndim = len(my_scan_params['dev_ids'])
-my_scan_params['gp_precisionmat'] = np.array([ [0.5]  ])
+my_scan_params['gp_precisionmat'] = np.array([ [0.5, 0.5]  ])
 
 #control device settings from which to start the scan, 1d-array of len(ndim)
 my_scan_params['start_point'] = np.array([1.0, 2.0])
 
 #UCB acquisition function parameters in order [nu, delta], list of length 2. If delta is None, ucb will do a fixed tail search using nu as a zscore
-my_scan_params['ucb_params'] = [1.0, None] 
+my_scan_params['ucb_params'] = [1.0, None]
+
+CheckScanParams(my_scan_params)   # perform some basic error handling
 
 np.save('../params/'+filename, my_scan_params)
